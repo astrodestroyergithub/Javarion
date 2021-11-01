ae78a748e7a98b533a9717a4a2502901b749035b
Tamojit Roy <67942951+astrodestroyergithub@users.noreply.github.com>
2021-11-01 21:14:05 +0530

    Added Hashtags Separator File

---
+++ HashtagsSeparator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class HashtagsSeparator {
+
+	public static void main(String[] args) {
+		//Initialize the String which needs to be split
+	    String str;
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter the hashtags");
+	    str = sc.nextLine();
+	    
+	    //Use the Split method and store the array of Strings returned in a String array
+	    String[] arr = str.split(" ");
+	    
+	    //Printing the characters using for-each loop
+	    for(String character : arr)
+	    {
+	    	System.out.print(character+",");
+	    }
+	}
+}
